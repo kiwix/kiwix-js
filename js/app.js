3f382a572a49d0a6d28963d9220fc0b2bd8c197f
mossroy <mossroy@free.fr>
2013-06-04 10:53:45 +0200

    Remove "Install" button In any case, Evopdia needs access to DeviceStorage, so it needs to be a privileged webapp. As such, it has to be signed and distributed through an app store. If I understood correctly, it will never be installable via un simple Install button on a web page. See https://wiki.mozilla.org/Apps/Security#Installed_privileged_application

--- js/app.js
+++ js/app.js
@@ -8,28 +8,6 @@ define(function(require) {
     // http://zeptojs.com/
     var $ = require('zepto');
 
-    var installBtn = document.getElementById('install-btn');
-    if(installBtn) {
-        installBtn.style.display = 'none';
-        // If you want an installation button, add this to your HTML:
-        //
-        // <button id="install-btn">Install</button>
-        //
-        // This code shows the button if the apps platform is available
-        // and this app isn't already installed.
-        if(navigator.mozApps) {
-            installBtn.addEventListener('click', function() {
-                navigator.mozApps.install(location.href + 'manifest.webapp');
-            }, false);
-            var req = navigator.mozApps.getSelf();
-            req.onsuccess = function() {
-                if(!req.result) {
-                    installBtn.style.display = 'block';
-                }
-            };
-        }
-    }
-
     // Evopedia javascript dependencies
     var evopedia = require('evopedia');
 
