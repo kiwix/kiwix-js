5a9f9af111784db2fbc14f15fff8632cc0506fcf
Peter <peter@exinto.de>
2016-10-03 16:59:36 +0200

    Fix tests.

--- browser-tests/nightwatch_runner.js
+++ browser-tests/nightwatch_runner.js
@@ -2,18 +2,11 @@ module.exports = {
     'All Tests': function(browser) {
         browser
             .url('http://localhost:8080/tests.html')
-            .waitForElementVisible('#qunit-testresult', 10000);
-        var resultsAvailable = false;
-        while (!resultsAvailable) {
-            browser
-                .pause(500)
-                .getText('#qunit-testresult', function(result) {
-                    resultsAvailable = result.search(/Tests completed in/);
-                });
-        }
-        browser
-            .expect.element('#qunit-testresult failed').text.to.equal('0')
-            .expect.element('#qunit-testresult passed').text.not.to.equal('0')
-            .end();
+            .waitForElementVisible('#qunit-testresult', 10000)
+            .pause(10000);
+        browser.expect.element('#qunit-testresult').text.to.contain('Tests completed in');
+        browser.expect.element('#qunit-testresult .failed').text.to.equal('0');
+        browser.expect.element('#qunit-testresult .passed').text.not.to.equal('0');
+        browser.end();
     }
 };
