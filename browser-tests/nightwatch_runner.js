14e486ade7ed862b6ccc475fc9bbe066e93ae6b7
sharun-s <sharun.msgs@gmail.com>
2017-05-20 12:55:00 +0530

    #131 Fix Review Changes browser-tests/nightwatch_runner.js - updated expected string to match new version QUnit tests.html - added a comment about how to run tests with Chrome tests/tests.js added xhr.status check to catch xhr errors removed unused errorHandlerAsyncTest fixed assert string typo in local.js can be loaded test

--- browser-tests/nightwatch_runner.js
+++ browser-tests/nightwatch_runner.js
@@ -4,7 +4,7 @@ module.exports = {
             .url('http://localhost:8080/tests.html')
             .waitForElementVisible('#qunit-testresult', 10000)
             .pause(10000);
-        browser.expect.element('#qunit-testresult').text.to.contain('Tests completed in');
+        browser.expect.element('#qunit-testresult').text.to.contain('tests completed in');
         browser.expect.element('#qunit-testresult .failed').text.to.equal('0');
         browser.expect.element('#qunit-testresult .passed').text.not.to.equal('0');
         browser.end();
