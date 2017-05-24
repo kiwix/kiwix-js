713aa94f3afed454b46fc922ac2b15a85e9b5a4d
Mossroy <mossroy@mossroy.fr>
2017-05-24 14:03:59 +0200

    Copy signed Firefox extension from previous nightly if already signed.
    Mozilla does not allow to sign a version that has already been signed.
    This is an issue in nightly builds when nothing has been commited in the
    last day.
    The workaround commited here is to copy the signed extension of this
    same version from other nightly folders
    Fixes #217

--- scripts/package_firefox_extension.sh
+++ scripts/package_firefox_extension.sh
@@ -15,4 +15,25 @@ zip -r ../build/kiwix-firefox-unsigned-extension-$VERSION.xpi www webextension m
 # Sign the extension with the Mozilla API through web-ext
 echo "Signing the extension for Firefox with Mozilla API, version $VERSION"
 ../node_modules/web-ext/bin/web-ext sign --api-key=${MOZILLA_API_KEY} --api-secret=${MOZILLA_API_SECRET}
-mv web-ext-artifacts/*.xpi ../build/kiwix-firefox-signed-extension-$VERSION.xpi
+
+# Check if the extension has been signed by Mozilla.
+# The reason signing usually fails is because the same version has already been signed by Mozilla.
+# So we try to find the signed extension of the same commit id in a previous nightly build
+FILECOUNT=$(find web-ext-artifacts -name '*.xpi' | wc -l)
+if [ $FILECOUNT -ge 1 ]; then
+        echo "Extension properly signed by Mozilla"
+	mv web-ext-artifacts/*.xpi ../build/kiwix-firefox-signed-extension-$VERSION.xpi
+else
+        echo "Extension not signed by Mozilla. It might be because this commit id has already been signed : let's look for it in a previous nightly build"
+	FOUND=0
+	for FILE in $(ssh -i ../scripts/travisci_builder_id_key nightlybot@download.kiwix.org "find /var/www/download.kiwix.org/nightly -name \"kiwix-firefox-signed-extension-$VERSION.xpi\""); do
+		echo "Signed extension found on the server in $FILE : copying it locally"
+		scp -i ../scripts/travisci_builder_id_key nightlybot@download.kiwix.org:$FILE ../build/
+		FOUND=1
+                # We only need the first matching file
+		break
+	done
+	if [ $FOUND -ne 1 ]; then
+		echo "Signed extension not found in a previous build"
+	fi
+fi
