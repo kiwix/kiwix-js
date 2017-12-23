8e767a76af323e4077b22cd0952de03037ad656e
Mossroy <mossroy@mossroy.fr>
2017-12-23 21:53:16 +0100

    Run automated unit and UI tests on several browsers.
    Latest versions of Firefox, Chrome and Edge
    Plus some older browsers : Firefox 45, Chrome 58 and IE11.
    For IE11, I had to disable unit tests (not supported).
    I also added a fix in UI tests that make them more stable,
    and added some file headers.

--- nightwatch.js
+++ nightwatch.js
@@ -1,5 +1,27 @@
-'use strict'
-var TRAVIS_JOB_NUMBER = process.env.TRAVIS_JOB_NUMBER
+/**
+ * nightwatch.js : Configuration of nightwatch.
+ * Global settings of NightWatch.
+ * 
+ * Copyright 2017 Mossroy and contributors
+ * License GPL v3:
+ * 
+ * This file is part of Kiwix.
+ * 
+ * Kiwix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Kiwix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Kiwix (file LICENSE-GPLv3.txt).  If not, see <http://www.gnu.org/licenses/>
+ */
+'use strict';
+var TRAVIS_JOB_NUMBER = process.env.TRAVIS_JOB_NUMBER;
 
 module.exports = {
   "src_folders" : ["browser-tests"],
@@ -24,10 +46,56 @@ module.exports = {
         "waitForConditionTimeout": 600
       }
     },
+    "firefox45" : {
+      "desiredCapabilities": {
+        "browserName": "firefox",
+        "version": "45.0",
+        "javascriptEnabled": true,
+        "acceptSslCerts": true,
+        "build": "build-" + TRAVIS_JOB_NUMBER,
+        "tunnel-identifier": TRAVIS_JOB_NUMBER
+      }
+    },
     "firefox" : {
       "desiredCapabilities": {
         "browserName": "firefox",
-        "version": "47.0", // Workaround some timeouts in nightwatch on Firefox>=49. See https://github.com/kiwix/kiwix-js/issues/251
+        "javascriptEnabled": true,
+        "acceptSslCerts": true,
+        "build": "build-" + TRAVIS_JOB_NUMBER,
+        "tunnel-identifier": TRAVIS_JOB_NUMBER
+      }
+    },
+    "chrome58" : {
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "version": "58.0",
+        "javascriptEnabled": true,
+        "acceptSslCerts": true,
+        "build": "build-" + TRAVIS_JOB_NUMBER,
+        "tunnel-identifier": TRAVIS_JOB_NUMBER
+      }
+    },
+    "chrome" : {
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true,
+        "acceptSslCerts": true,
+        "build": "build-" + TRAVIS_JOB_NUMBER,
+        "tunnel-identifier": TRAVIS_JOB_NUMBER
+      }
+    },
+    "edge" : {
+      "desiredCapabilities": {
+        "browserName": "MicrosoftEdge",
+        "javascriptEnabled": true,
+        "acceptSslCerts": true,
+        "build": "build-" + TRAVIS_JOB_NUMBER,
+        "tunnel-identifier": TRAVIS_JOB_NUMBER
+      }
+    },
+    "ie11" : {
+      "desiredCapabilities": {
+        "browserName": "internet explorer",
         "javascriptEnabled": true,
         "acceptSslCerts": true,
         "build": "build-" + TRAVIS_JOB_NUMBER,
@@ -35,4 +103,4 @@ module.exports = {
       }
     }
   }
-}
+};
