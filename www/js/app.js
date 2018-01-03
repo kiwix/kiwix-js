f6459dc1bc4d623d07f504ff2a8d12c898c63b13
Mossroy <mossroy@mossroy.fr>
2018-01-03 11:49:23 +0100

    Fix compatibility with IE11.
    IE11 does not support webRequests in any case, but the syntax used in it
    was breaking it in other modes as well.

--- www/js/app.js
+++ www/js/app.js
@@ -345,7 +345,7 @@ define(['jquery', 'zimArchiveLoader', 'util', 'uiUtil', 'cookies','abstractFiles
         let decoder = new TextDecoder("utf-8");
         let encoder = new TextEncoder();
 
-        filter.ondata = event => {
+        filter.ondata = function(event) {
             let str = decoder.decode(event.data, {stream: true});
             // Just change any instance of Kiwix in the HTTP response
             // to something else.
