88dd4d6e65de594cb634c178a313662f5d304d5d
Mossroy <mossroy@mossroy.fr>
2018-09-22 13:50:04 +0200

    Change version numbers to prepare next version 2.5

--- scripts/create_all_packages.sh
+++ scripts/create_all_packages.sh
@@ -12,8 +12,8 @@ while getopts tdv: option; do
     esac
 done
 
-MAJOR_NUMERIC_VERSION="2.4"
-VERSION_TO_REPLACE="2\.4-WIP"
+MAJOR_NUMERIC_VERSION="2.5"
+VERSION_TO_REPLACE="2\.5-WIP"
 
 # Set the secret environment variables if available
 # The file set_secret_environment_variables.sh should not be commited for security reasons
