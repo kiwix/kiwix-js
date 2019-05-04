2c61d25b23cbd2b72584aef94a41e6e2c5bbac03
Jaifroid <egk10@cam.ac.uk>
2019-05-04 21:02:05 +0100

    Add error function

--- www/js/lib/zimfile.js
+++ www/js/lib/zimfile.js
@@ -120,7 +120,7 @@ define(['xzdec_wrapper', 'util', 'utf8', 'q', 'zimDirEntry'], function(xz, util,
     ZIMFile.prototype._mimeTypeMap = function() {
         var typeMap = new Map;
         return this._readSlice(this.mimeListPos, 256).then(function(data) {
-            // DEV: We have read 256 bytes: increase this if you encounter longer MIME Type lists
+            // DEV: We have read 256 bytes: increase this if you encounter longer MIME type lists
             // also change "while (pos < 255)" below
             if (data.subarray) {
                 var i = 1;
@@ -129,7 +129,7 @@ define(['xzdec_wrapper', 'util', 'utf8', 'q', 'zimDirEntry'], function(xz, util,
                 while (pos < 255) {
                     pos++; 
                     mimeString = utf8.parse(data.subarray(pos), true);
-                    // If the parsed data is an empty string, we have reached the end of the MIME Type list, so break 
+                    // If the parsed data is an empty string, we have reached the end of the MIME type list, so break 
                     if (!mimeString) break;
                     // Store the parsed string in the Map
                     typeMap.set(i, mimeString);
@@ -140,6 +140,9 @@ define(['xzdec_wrapper', 'util', 'utf8', 'q', 'zimDirEntry'], function(xz, util,
                 }
             }
             return typeMap;
+        }).fail(function(err) {
+            console.errror('Unable to read MIME type list: ' + err);
+            return new Map;
         });
     };
     
