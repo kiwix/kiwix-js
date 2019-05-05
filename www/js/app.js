d32be8a51d9077aacf28935be94d31bf7c7a277f
Jaifroid <egk10@cam.ac.uk>
2019-05-05 11:02:41 +0100

    Spacing and hide/show logic

--- www/js/app.js
+++ www/js/app.js
@@ -601,6 +601,7 @@ define(['jquery', 'zimArchiveLoader', 'util', 'uiUtil', 'cookies','abstractFiles
             document.getElementById('openLocalFiles').style.display = 'none';
             document.getElementById('downloadInstruction').style.display = 'none';
             document.getElementById('selectorsDisplay').style.display = 'block';
+            document.getElementById('selectorsDisplayLink').style.display = 'inline';
             document.getElementById('selectorsDisplayLink').addEventListener('click', function(e) {
                 e.preventDefault();
                 document.getElementById('openLocalFiles').style.display = 'block';
@@ -611,6 +612,7 @@ define(['jquery', 'zimArchiveLoader', 'util', 'uiUtil', 'cookies','abstractFiles
         selectedArchive = zimArchiveLoader.loadArchiveFromFiles(files, function (archive) {
             // The archive is set : go back to home page to start searching
             $("#btnHome").click();
+            document.getElementById('downloadInstruction').style.display = 'none';
         });
     }
     /**
