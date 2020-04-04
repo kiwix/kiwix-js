ab47958c9438d74685688d7215e72a1af1adf020
Jaifroid <egk10@cam.ac.uk>
2020-04-04 16:25:32 +0100

    Support localStorage for storing settings

--- www/js/lib/cookies.js
+++ www/js/lib/cookies.js
@@ -21,11 +21,35 @@ define([], function() {
 |*|
 \*/
 
+// Test for cookie support
+var storeType = 'cookie';
+document.cookie = 'kiwixCookie=working;expires=Fri, 31 Dec 9999 23:59:59 GMT';
+var kiwixCookie = /kiwixCookie=working/i.test(document.cookie);
+if (kiwixCookie) {
+    document.cookie = 'kiwixCookie=broken;expires=Fri, 31 Dec 9999 23:59:59 GMT';
+    kiwixCookie = !/kiwixCookie=working/i.test(document.cookie);
+}
+document.cookie = 'kiwixCookie=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+// Test for localStorage support
+var localStorageTest = false;
+try {
+    localStorageTest = 'localStorage' in window && window['localStorage'] !== null;
+} catch (e) {
+    console.log('LocalStorage is not supported!');
+}
+if (localStorageTest) storeType = 'local_storage';
+console.log('cookiesTest: storeType: ' + storeType);
+
 var docCookies = {
   getItem: function (sKey) {
+    if (storeType === 'cookie') {
     return unescape(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    } else {
+      return localStorage.getItem(sKey);
+    }
   },
   setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+    if (storeType === 'cookie') {
     if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
     var sExpires = "";
     if (vEnd) {
@@ -42,11 +66,18 @@ var docCookies = {
       }
     }
     document.cookie = escape(sKey) + "=" + escape(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+    } else {
+      localStorage.setItem(sKey, sValue);
+    }
     return true;
   },
   removeItem: function (sKey, sPath) {
+    if (storeType === 'cookie') {
     if (!sKey || !this.hasItem(sKey)) { return false; }
     document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sPath ? "; path=" + sPath : "");
+    } else {
+      localStorage.removeItem(sKey);
+    } 
     return true;
   },
   hasItem: function (sKey) {
