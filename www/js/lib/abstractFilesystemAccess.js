354f25e81bc883ab6c6b821ce79382660fe9f841
Mossroy <mossroy@mossroy.fr>
2021-07-17 21:52:55 +0200

    Remove unnecessary dependency on jQuery

--- www/js/lib/abstractFilesystemAccess.js
+++ www/js/lib/abstractFilesystemAccess.js
@@ -25,7 +25,7 @@
  * along with Kiwix (file LICENSE-GPLv3.txt).  If not, see <http://www.gnu.org/licenses/>
  */
 'use strict';
-define(['jquery'], function(jQuery) {
+define([], function() {
     
     /**
      * Storage implemented by Firefox OS
