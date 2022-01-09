7be1d5f576f01cc90b5d784d4f389b4fec723527
Manjit Kaur Khaira <81208462+Maneet-k@users.noreply.github.com>
2022-01-09 22:44:27 +0530

    update uiUtil.js

--- www/js/lib/uiUtil.js
+++ www/js/lib/uiUtil.js
@@ -348,12 +348,10 @@ define(rqDef, function() {
      * @param {String} theme The theme to apply (light|dark[_invert|_mwInvert])
      */
     function applyAppTheme(theme) {
-        if (theme === "auto_invert") {
-            theme = window.matchMedia('(prefers-color-scheme:dark)').matches ? "dark_invert" : "light" 
-        }
-        if (theme === "auto_mwInvert") {
-            theme = window.matchMedia('(prefers-color-scheme:dark)').matches ? "dark_mwInvert" : "light" 
-        }
+        if(!window.matchMedia) document.getElementById('appThemeSelect').options[0].style.display = "none";
+        if(!window.matchMedia) document.getElementById('appThemeSelect').options[1].style.display = "none";
+        if (theme === "auto_invert") theme = window.matchMedia('(prefers-color-scheme:dark)').matches ? "dark_invert" : "light" 
+        if (theme === "auto_mwInvert") theme = window.matchMedia('(prefers-color-scheme:dark)').matches ? "dark_mwInvert" : "light" 
         var htmlEl = document.querySelector('html');
         var footer = document.querySelector('footer');
         var oldTheme = htmlEl.dataset.theme || '';
