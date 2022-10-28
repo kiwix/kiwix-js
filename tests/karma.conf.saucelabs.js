edceaf769e1121911b5e5c787e5339116451bea8
Jaifroid <egk10@cam.ac.uk>
2022-10-28 06:45:15 +0100

    Update karma.conf.saucelabs.js

--- tests/karma.conf.saucelabs.js
+++ tests/karma.conf.saucelabs.js
@@ -15,11 +15,11 @@ module.exports = function (config) {
         browserName: 'firefox',
         version: '52.0'
       },
-      firefox: {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        version: '100'
-      },
+      // firefox: {
+      //   base: 'SauceLabs',
+      //   browserName: 'firefox',
+      //   version: '100'
+      // },
       chrome58: {
         base: 'SauceLabs',
         browserName: 'chrome',
@@ -56,7 +56,7 @@ module.exports = function (config) {
 
     // REMINDER: Keep this list in sync with the UI tests, in .github/workflows/CI.yml.
     browsers: [
-      'firefox',
+      // 'firefox',
       'chrome',
       'edge',
       'edge40',
