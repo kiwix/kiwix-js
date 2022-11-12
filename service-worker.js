a63294d731e75eb50568544a29788eb24d519f20
Jaifroid <egk10@cam.ac.uk>
2022-11-12 14:15:24 +0000

    Source code for v3.7-WIP

--- service-worker.js
+++ service-worker.js
@@ -29,7 +29,7 @@
  * download and install a new copy; we have to hard code this here because it is needed before any other file
  * is cached in APP_CACHE
  */
-const appVersion = '3.6-WIP';
+const appVersion = '3.7-WIP';
 
 /**
  * The name of the Cache API cache in which assets defined in regexpCachedContentTypes will be stored
