cc263f6cf5b170e7e20452d893d8209a7546485a
Jaifroid <egk10@cam.ac.uk>
2022-11-27 11:55:59 +0000

    Workaround: don't instantiate libzim if the archive is split

--- www/js/lib/zimArchive.js
+++ www/js/lib/zimArchive.js
@@ -87,11 +87,14 @@ define(['zimfile', 'zimDirEntry', 'util', 'utf8'],
                         countName: 'fullTextIndexSize'
                     }
                 ]).then(function () {
-                    if ('WebAssembly' in self && that._file.fullTextIndex) {
+                    var isSplitZim = /\.zima.$/i.test(that._file._files[0].name);
+                    if ('WebAssembly' in self && that._file.fullTextIndex && !isSplitZim) {
                         console.log('Instantiating libzim Web Worker...');
                         libzimWorker = new Worker('js/lib/libzim-wasm.js');
                         that.callLibzimWorker({action: "init", files: that._file._files});
-                    }
+                    } else {
+                        if (isSplitZim) console.warn('Full text searching was disabled because ZIM archive is split.');
+                    };
                 });
                 // Set the archive file type ('open' or 'zimit')
                 that.setZimType();
