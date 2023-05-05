4cfae83c77166617575578ae596b828438980d4a
Jaifroid <egk10@cam.ac.uk>
2023-05-05 07:59:15 +0100

    Add PWA ServiceWorker mode for Chromium extension (#1000)

--- www/js/app.js
+++ www/js/app.js
@@ -186,6 +186,10 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'settingsStore','abstractFilesys
         params.defaultModeChangeAlertDisplayed = true;
         settingsStore.setItem('defaultModeChangeAlertDisplayed', true, Infinity);
     }
+    if (!/^chrome-extension:/i.test(window.location.protocol)) {
+        document.getElementById('serviceWorkerLocal').style.display = 'none';
+        document.getElementById('serviceWorkerLocalDescription').style.display = 'none';
+    }
     setContentInjectionMode(params.contentInjectionMode);
 
     // Define globalDropZone (universal drop area) and configDropZone (highlighting area on Config page)
@@ -815,6 +819,12 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'settingsStore','abstractFilesys
      * @param {String} value The chosen content injection mode : 'jquery' or 'serviceworker'
      */
     function setContentInjectionMode(value) {
+        params.oldInjectionMode = params.serviceWorkerLocal ? 'serviceworkerlocal' : params.contentInjectionMode;
+        params.serviceWorkerLocal = false;
+        if (value === 'serviceworkerlocal') {
+            value = 'serviceworker';
+            params.serviceWorkerLocal = true;
+        }
         params.contentInjectionMode = value;
         if (value === 'jquery') {
             if (!params.appCache) {
@@ -867,7 +877,7 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'settingsStore','abstractFilesys
             // Previously, the API was available, but failed to register (which we could trap a few lines below).
             // So we now need to suggest a switch to the PWA if we are inside a Firefox Extension and the ServiceWorker API is unavailable.
             // Even if some older firefox versions do not support ServiceWorkers at all (versions 42, 43, 45ESR, 52ESR, 60ESR and 68ESR, based on https://caniuse.com/serviceworkers). In this case, the PWA will not work either.
-            if (protocol === 'moz-extension:' && !isServiceWorkerAvailable()) {
+            if (/^(moz|chrome)-extension:/.test(protocol) && !params.serviceWorkerLocal) {
                 launchMozillaExtensionServiceWorker();
             } else {
                 if (!isServiceWorkerAvailable()) {
@@ -881,7 +891,7 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'settingsStore','abstractFilesys
                             var uriParams = '?allowInternetAccess=false&contentInjectionMode=jquery&defaultModeChangeAlertDisplayed=true';
                             window.location.href = params.referrerExtensionURL + '/www/index.html' + uriParams;
                         } else {
-                            setContentInjectionMode('jquery');
+                            setContentInjectionMode(params.oldInjectionMode || 'jquery');
                         }
                     });
                     return;
@@ -964,9 +974,10 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'settingsStore','abstractFilesys
             resetCssCache();
         }
         $('input:radio[name=contentInjectionMode]').prop('checked', false);
-        $('input:radio[name=contentInjectionMode]').filter('[value="' + value + '"]').prop('checked', true);
+        var trueMode = params.serviceWorkerLocal ? value + 'local' : value;
+        $('input:radio[name=contentInjectionMode]').filter('[value="' + trueMode + '"]').prop('checked', true);
         // Save the value in the Settings Store, so that to be able to keep it after a reload/restart
-        settingsStore.setItem('contentInjectionMode', value, Infinity);
+        settingsStore.setItem('contentInjectionMode', trueMode, Infinity);
         refreshCacheStatus();
         refreshAPIStatus();
         // Set the visibility of WebP workaround after change of content injection mode
@@ -1046,7 +1057,7 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'settingsStore','abstractFilesys
                     '<br/><br/>(Kiwix needs one-time access to the server to cache the PWA).' +
                     '<br/>Please try again when you have a stable Internet connection.', 'Error!').then(function () {
                         settingsStore.setItem('allowInternetAccess', false, Infinity);
-                        setContentInjectionMode('jquery');
+                        setContentInjectionMode(params.oldInjectionMode || 'jquery');
                     });
             });
         };
@@ -1059,7 +1070,7 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'settingsStore','abstractFilesys
                         checkPWAIsOnline();
                     } else {
                         settingsStore.setItem('allowInternetAccess', false, Infinity);
-                        setContentInjectionMode('jquery');
+                        setContentInjectionMode(params.oldInjectionMode || 'jquery');
                     }
                 })
             }
@@ -1068,8 +1079,9 @@ define(['jquery', 'zimArchiveLoader', 'uiUtil', 'settingsStore','abstractFilesys
                 if (response) {
                     checkPWAIsOnline();
                 } else {
-                    // User cancelled, so wants to stay in JQuery mode
-                    setContentInjectionMode('jquery');
+                    // User cancelled, so wants to stay in previous mode (so long as this wasn't SW mode)
+                    params.oldInjectionMode = params.oldInjectionMode === 'serviceworker' ? /^chrome-extension:/i.test(window.location.protocol) ? 'serviceworkerlocal' : null : params.oldInjectionMode;
+                    setContentInjectionMode(params.oldInjectionMode || 'jquery');
                     settingsStore.setItem('allowInternetAccess', false, Infinity);
                     // We should not bother user with the default mode change alert again
                     params.defaultModeChangeAlertDisplayed = true;
