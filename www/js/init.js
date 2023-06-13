930b0fae5c980cefafb92dbbb38ad1c9a7ab9e93
Jaifroid <egk10@cam.ac.uk>
2023-06-13 07:06:13 +0100

    Fix some ESLint issues in app.js and init.js (#1016)
    * Fix no-trailing spaces
    
    * Fix no quoted properties
    
    * Fix inconsistent quotes in app.js
    
    * Fix various spacing issues in app.js
    
    * Fix issues in init.js

--- www/js/init.js
+++ www/js/init.js
@@ -1,31 +1,33 @@
 /**
  * init.js : Configuration for the library require.js
  * This file handles the dependencies between javascript libraries
- * 
+ *
  * Copyright 2013-2020 Mossroy and contributors
  * License GPL v3:
- * 
+ *
  * This file is part of Kiwix.
- * 
+ *
  * Kiwix is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Kiwix is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Kiwix (file LICENSE-GPLv3.txt).  If not, see <http://www.gnu.org/licenses/>
  */
 'use strict';
 
+/* global requirejs */
+
 /**
  * A global parameter object for storing variables that need to be remembered between page loads,
  * or across different functions and modules
- * 
+ *
  * @type Object
  */
 var params = {};
@@ -43,24 +45,23 @@ if (/PWA_launch=/.test(window.location.search)) {
     if (match[1] === 'success') localStorage.setItem(params.keyPrefix + 'defaultModeChangeAlertDisplayed', true);
     console.warn('Launch of PWA has been registered as "' + match[1] + '" by the extension. Exiting local code.');
 } else {
-
     require.config({
         baseUrl: 'js/lib',
         paths: {
-            'jquery': 'jquery-3.7.0.slim.min',
-            'bootstrap': 'bootstrap.bundle',
-            'webpHeroBundle': 'webpHeroBundle_0.0.2',
-            'fontawesome': 'fontawesome/fontawesome',
+            jquery: 'jquery-3.7.0.slim.min',
+            bootstrap: 'bootstrap.bundle',
+            webpHeroBundle: 'webpHeroBundle_0.0.2',
+            fontawesome: 'fontawesome/fontawesome',
             'fontawesome-solid': 'fontawesome/solid'
         },
         shim: {
-            'jquery': {
+            jquery: {
                 exports: '$'
             },
-            'bootstrap': {
+            bootstrap: {
                 deps: ['jquery', 'fontawesome', 'fontawesome-solid']
             },
-            'webpHeroBundle': ''
+            webpHeroBundle: ''
         }
     });
 
@@ -91,5 +92,4 @@ if (/PWA_launch=/.test(window.location.search)) {
             webpMachine = true;
         }
     });
-
 }
