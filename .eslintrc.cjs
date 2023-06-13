3e1dec90ad735e7b739151205f2a2feec606cc8a
Jaifroid <egk10@cam.ac.uk>
2023-06-13 20:14:16 +0100

    Do not warn about object-shorthand

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
         'no-extra-parens': 1,
         'no-unused-expressions': 1,
         'no-unused-vars': 1,
-        'n/no-callback-literal': 0
+        'n/no-callback-literal': 0,
+        'object-shorthand': 0
     }
 }
