19d294f478c39ebec433af2305ac0e5a94be9a90
Jaifroid <egk10@cam.ac.uk>
2023-06-14 06:38:10 +0100

    Fix zimArchiveLoader.js

--- www/js/lib/zimArchiveLoader.js
+++ www/js/lib/zimArchiveLoader.js
@@ -19,73 +19,76 @@
  * You should have received a copy of the GNU General Public License
  * along with Kiwix (file LICENSE-GPLv3.txt).  If not, see <http://www.gnu.org/licenses/>
  */
+
 'use strict';
+
+/* global define */
+
 define(['zimArchive', 'jquery'],
-       function(zimArchive, jQuery) {
+    function (zimArchive, jQuery) {
+        /**
+         * Create a ZIMArchive from DeviceStorage location
+         * @param {DeviceStorage} storage
+         * @param {String} path
+         * @param {callbackZIMArchive} callbackReady
+         * @param {callbackZIMArchive} callbackError
+         * @returns {ZIMArchive}
+         */
+        function loadArchiveFromDeviceStorage (storage, path, callbackReady, callbackError) {
+            return new zimArchive.ZIMArchive(storage, path, callbackReady, callbackError);
+        };
+        /**
+         * Create a ZIMArchive from Files
+         * @param {Array.<File>} files
+         * @param {callbackZIMArchive} callbackReady
+         * @param {callbackZIMArchive} callbackError
+         * @returns {ZIMArchive}
+         */
+        function loadArchiveFromFiles (files, callbackReady, callbackError) {
+            if (files.length >= 1) {
+                return new zimArchive.ZIMArchive(files, null, callbackReady, callbackError);
+            }
+        };
 
-    /**
-     * Create a ZIMArchive from DeviceStorage location
-     * @param {DeviceStorage} storage
-     * @param {String} path
-     * @param {callbackZIMArchive} callbackReady
-     * @param {callbackZIMArchive} callbackError
-     * @returns {ZIMArchive}
-     */
-    function loadArchiveFromDeviceStorage(storage, path, callbackReady, callbackError) {
-        return new zimArchive.ZIMArchive(storage, path, callbackReady, callbackError);
-    };
-    /**
-     * Create a ZIMArchive from Files
-     * @param {Array.<File>} files
-     * @param {callbackZIMArchive} callbackReady
-     * @param {callbackZIMArchive} callbackError
-     * @returns {ZIMArchive}
-     */
-    function loadArchiveFromFiles(files, callbackReady, callbackError) {
-        if (files.length >= 1) {
-            return new zimArchive.ZIMArchive(files, null, callbackReady, callbackError);
-        }
-    };
-    
-    /**
-     * @callback callbackPathList
-     * @param {Array.<String>} directoryList List of directories
-     */
-    
-    
-    /**
-     *  Scans the DeviceStorage for archives
-     *
-     * @param {Array.<DeviceStorage>} storages List of DeviceStorage instances
-     * @param {callbackPathList} callbackFunction Function to call with the list of directories where archives are found
-     * @param {callbackPathList} callbackError Function to call in case of an error
-     */
-    function scanForArchives(storages, callbackFunction, callbackError) {
-        var directories = [];
-        var promises = jQuery.map(storages, function(storage) {
-            return storage.scanForArchives()
-                .then(function(dirs) {
-                    jQuery.merge(directories, dirs);
-                    return true;
-                });
-        });
-        jQuery.when.apply(null, promises).then(function() {
-            callbackFunction(directories);
-        }).catch(function (error) {
-            callbackError("Error scanning your device storage : " + error
-            + ". If you're using the Firefox OS Simulator, please put the archives in "
-            + "a 'fake-sdcard' directory inside your Firefox profile "
-            + "(ex : ~/.mozilla/firefox/xxxx.default/extensions/fxos_2_x_simulator@mozilla.org/"
-            + "profile/fake-sdcard/wikipedia_en_ray_charles_2015-06.zim)", "Error reading Device Storage");
-        });
-    };
+        /**
+         * @callback callbackPathList
+         * @param {Array.<String>} directoryList List of directories
+         */
 
-    /**
-     * Functions and classes exposed by this module
-     */
-    return {
-        loadArchiveFromDeviceStorage: loadArchiveFromDeviceStorage,
-        loadArchiveFromFiles: loadArchiveFromFiles,
-        scanForArchives: scanForArchives
-    };
-});
+        /**
+         *  Scans the DeviceStorage for archives
+         *
+         * @param {Array.<DeviceStorage>} storages List of DeviceStorage instances
+         * @param {callbackPathList} callbackFunction Function to call with the list of directories where archives are found
+         * @param {callbackPathList} callbackError Function to call in case of an error
+         */
+        function scanForArchives (storages, callbackFunction, callbackError) {
+            var directories = [];
+            var promises = jQuery.map(storages, function (storage) {
+                return storage.scanForArchives()
+                    .then(function (dirs) {
+                        jQuery.merge(directories, dirs);
+                        return true;
+                    });
+            });
+            jQuery.when.apply(null, promises).then(function () {
+                callbackFunction(directories);
+            }).catch(function (error) {
+                callbackError('Error scanning your device storage : ' + error +
+                ". If you're using the Firefox OS Simulator, please put the archives in " +
+                "a 'fake-sdcard' directory inside your Firefox profile " +
+                '(ex : ~/.mozilla/firefox/xxxx.default/extensions/fxos_2_x_simulator@mozilla.org/' +
+                'profile/fake-sdcard/wikipedia_en_ray_charles_2015-06.zim)', 'Error reading Device Storage');
+            });
+        };
+
+        /**
+         * Functions and classes exposed by this module
+         */
+        return {
+            loadArchiveFromDeviceStorage: loadArchiveFromDeviceStorage,
+            loadArchiveFromFiles: loadArchiveFromFiles,
+            scanForArchives: scanForArchives
+        };
+    }
+);
