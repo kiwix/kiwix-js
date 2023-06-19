31d9963653a8528b3a1e71157d08ba8d3c6d2b02
Jaifroid <egk10@cam.ac.uk>
2023-06-19 09:14:51 +0100

    Import jQuery directly

--- www/js/lib/jquery.module.js
+++ www/js/lib/jquery.module.js
@@ -2,5 +2,5 @@
  * Converter module for jQuery. See https://stackoverflow.com/questions/34338411/how-to-import-jquery-using-es6-syntax
  */
 
-import 'jquery';
+import '../../../node_modules/jquery/dist/jquery.slim.js';
 export default window.$;
