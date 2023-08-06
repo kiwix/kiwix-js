9d986cde17fe8d38cefa7ec9e081ba7de4d22b60
Jaifroid <egk10@cam.ac.uk>
2023-08-06 15:02:04 +0300

    Add headless CI for Windows

--- tests/chromium.e2e.runner.js
+++ tests/chromium.e2e.runner.js
@@ -1,13 +1,22 @@
 import { Builder } from 'selenium-webdriver';
+import { Options } from 'selenium-webdriver/chrome.js';
 import e2e from './kiwix-js.e2e.spec.js';
 
 /* eslint-disable camelcase */
 
 async function loadChromiumDriver () {
-    const driver = await new Builder().forBrowser('chrome').build();
+    const options = new Options();
+    // Run it headless if the environment variable GITHUB_ACTIONS is set
+    if (process.env.GITHUB_ACTIONS) {
+        options.addArguments('--headless=new');
+    }
+    const driver = await new Builder(options)
+        .forBrowser('chrome')
+        .setChromeOptions(options)
+        .build();
     return driver;
 };
 
-const driver_fx = await loadChromiumDriver();
+const driver_chrome = await loadChromiumDriver();
 
-e2e.runTests(driver_fx);
+e2e.runTests(driver_chrome);
--- tests/ieMode.e2e.runner.js
+++ tests/ieMode.e2e.runner.js
@@ -8,7 +8,6 @@ async function loadIEModeDriver () {
     const ieOptions = new Options();
     ieOptions.setEdgeChromium(true);
     ieOptions.setEdgePath('C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe');
-
     const driver = await new Builder()
         .forBrowser('ie')
         .setIeOptions(ieOptions)
