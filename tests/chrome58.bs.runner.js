1940b218ad4c3baa54b12ffd509a876bc85112be
Jaifroid <egk10@cam.ac.uk>
2023-08-13 14:28:35 +0100

    Add tests on Mojave

---
+++ tests/chrome58.bs.runner.js
@@ -0,0 +1,37 @@
+import { Builder } from 'selenium-webdriver';
+import legacyRayCharles from './legacy-ray_charles.e2e.spec.js';
+
+/* eslint-disable camelcase */
+
+// Input capabilities
+const capabilities = {
+    'bstack:options': {
+        os: 'OS X',
+        osVersion: 'Mojave',
+        browserVersion: '58.0',
+        projectName: 'BStack Project Name: Kiwix JS e2e tests',
+        buildName: 'BStack Build Name: Chrome 58 on Mojave',
+        local: true,
+        localIdentifier: process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
+        userName: process.env.BROWSERSTACK_USERNAME,
+        accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
+        seleniumVersion: '3.141.59'
+    },
+    browserName: 'Chrome'
+};
+
+async function loadChromeDriver () {
+    const driver = await new Builder()
+        // .forBrowser('edge')
+        .usingServer('https://hub-cloud.browserstack.com/wd/hub')
+        .withCapabilities(capabilities)
+        .build();
+    return driver;
+};
+
+const driver_chrome = await loadChromeDriver();
+
+// Maximize the window so that full browser state is visible in the screenshots
+await driver_chrome.manage().window().maximize();
+
+legacyRayCharles.runTests(driver_chrome);
