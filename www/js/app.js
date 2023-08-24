2e6fef829ffaad2579dfa5ecf66be74c8d383aae
Jaifroid <egk10@cam.ac.uk>
2023-08-24 14:39:25 +0100

    Translation cleanup and UI touches (#1067)

--- www/js/app.js
+++ www/js/app.js
@@ -78,7 +78,7 @@ var selectedArchive = null;
  * WARNING: Only change these paramaeters if you know what you are doing
  */
 // The current version number of this app
-params['appVersion'] = '3.9.1'; // **IMPORTANT** Ensure this is the same as the version number in service-worker.js
+params['appVersion'] = '3.9.3'; // **IMPORTANT** Ensure this is the same as the version number in service-worker.js
 // The PWA server (for use with the browser extensions in ServiceWorker mode)
 params['PWAServer'] = 'https://browser-extension.kiwix.org/current/'; // Include final slash!
 // params['PWAServer'] = 'https://kiwix.github.io/kiwix-js/'; // DEV: Uncomment this line for testing code on GitHub Pages
