2a28b735f4611d069e53a057cab7ce583a01e824
Jaifroid <egk10@cam.ac.uk>
2023-11-12 16:34:17 +0000

    Translation corrections and add tooltip explaining library (#1155)

--- i18n/fr.jsonp.js
+++ i18n/fr.jsonp.js
@@ -19,6 +19,7 @@ document.localeJson = {
       "configure-title": "Configuration",
       "configure-about-usage-link": "Informations (Utilisation)",
       "configure-btn-library": "Bibliothèque ZIM",
+      "configure-btn-library-tip": "Cela affiche une bibliothèque graphique d'archives ZIM à télécharger si votre navigateur peut l'ouvrir. Sinon, il affiche une bibliothèque de téléchargement en mode texte. Si vous utilisez l'extension Chromium en mode ServiceWorkerLocal, vous ne pourrez utiliser que la seconde (passez en mode ServiceWorker pour la version graphique complète).",
       "configure-btn-folderselect": "Sélectionner un dossier",
       "configure-btn-rescan": "Réanalyser",
       "configure-about-rescan-tip": "Réanalyser la carte SD et la mémoire interne",
