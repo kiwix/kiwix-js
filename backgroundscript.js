f5a7681020db2dae1d9d7f39b105eb898478ffb8
Jaifroid <egk10@cam.ac.uk>
2023-11-13 10:52:01 +0000

    Fix invocation

--- backgroundscript.js
+++ backgroundscript.js
@@ -34,7 +34,7 @@ if (typeof browser !== 'undefined') {
 }
 // DEV: For a Mozilla MV3 extension, we have to use 'action' instead of 'browserAction'
 var genericAction = genericBrowser.browserAction || genericBrowser.action;
-genericBrowser[genericAction].onClicked.addListener(function () {
+genericAction.onClicked.addListener(function () {
     var newURL = chrome.runtime.getURL('www/index.html');
     chrome.tabs.create({ url: newURL });
 });
