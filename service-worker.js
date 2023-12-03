c439209fe0b56dbac512fbfaa4228b238fd24f19
Jaifroid <egk10@cam.ac.uk>
2023-12-03 09:40:58 +0000

    Bump appVersion to pre-release 3.11.5

--- service-worker.js
+++ service-worker.js
@@ -33,7 +33,7 @@
  * download and install a new copy; we have to hard code this here because it is needed before any other file
  * is cached in APP_CACHE
  */
-const appVersion = '3.11.2';
+const appVersion = '3.11.5';
 
 /**
  * The name of the Cache API cache in which assets defined in regexpCachedContentTypes will be stored
