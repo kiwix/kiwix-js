92e6177cd10b54a5e9172c1dfce895b2b0cb1724
Jaifroid <egk10@cam.ac.uk>
2024-02-12 12:55:22 +0000

    Bump app version for testing extensions

--- www/js/init.js
+++ www/js/init.js
@@ -79,7 +79,7 @@ var params = {};
  * WARNING: Only change these parameters if you know what you are doing
  */
 // The current version number of this app
-params['appVersion'] = '3.11.5'; // **IMPORTANT** Ensure this is the same as the version number in service-worker.js
+params['appVersion'] = '3.11.9'; // **IMPORTANT** Ensure this is the same as the version number in service-worker.js
 // The PWA server (for use with the browser extensions in ServiceWorker mode)
 params['PWAServer'] = 'https://browser-extension.kiwix.org/current/'; // Include final slash!
 // params['PWAServer'] = 'https://kiwix.github.io/kiwix-js/'; // DEV: Uncomment this line for testing code on GitHub Pages
