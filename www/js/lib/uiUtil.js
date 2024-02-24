c3920726f65c1c84128241967780d178aca03c89
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-02-24 18:12:58 +0530

    update app and uiutil

--- www/js/lib/uiUtil.js
+++ www/js/lib/uiUtil.js
@@ -454,7 +454,7 @@ var activeContentWarningSetup = false;
 function displayActiveContentWarning (mode) {
     // Adapt the text for other modes (e.g., ServiceWorkerLocal)
     if (mode) {
-        activeContent.innerHTML = activeContent.innerHTML.replace(/JQuery/i, mode);
+        activeContent.innerHTML = activeContent.innerHTML.replace(/Safe/i, mode);
         // We have to set up event listeners again
         activeContentWarningSetup = false;
     }
