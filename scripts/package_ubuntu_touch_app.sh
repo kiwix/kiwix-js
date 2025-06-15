4a906919274f27a5ad50c18fa5a33400dacf906c
Jaifroid <egk10@cam.ac.uk>
2025-06-15 22:46:30 +0100

    Update Ubuntu Touch sdk to 20.04 #1353 (#1354)

--- scripts/package_ubuntu_touch_app.sh
+++ scripts/package_ubuntu_touch_app.sh
@@ -18,12 +18,28 @@ while getopts tdv: option; do
     esac
 done
 
-echo "Packaging .click application for Ubuntu Touch, version $VERSION"
+echo "Packaging .click application for Ubuntu Touch using Clickable, version $VERSION"
 
-# We need to remove the *.woff* files because click considers they are forbidden binaries
-rm -f "$BASEDIR/tmp/www/fonts"/*.woff*
-# We need to remove these files because the --ignore option of click is not supported in the version of Trusty
-rm -rf "$BASEDIR/tmp/webextension"
-rm -f "$BASEDIR/tmp/manifest.webapp"
-click build --no-validate "$BASEDIR/tmp"
-mv kiwix*.click $BASEDIR/build/kiwix-ubuntu-touch-$VERSION.click
+# Copy the clickable.yaml configuration file to the tmp directory
+cp "$BASEDIR/ubuntu_touch/clickable.yaml" "$BASEDIR/tmp/"
+
+# Show clickable version for debugging
+echo "Clickable version:"
+clickable --version
+
+# Build with Clickable using the pure builder
+echo "Building with Clickable (pure builder for web apps)..."
+clickable build
+
+# Find the generated click file
+CLICK_FILE=$(find "$BASEDIR/tmp" -name "*.click" -type f | head -1)
+
+if [ -n "$CLICK_FILE" ]; then
+    mv "$CLICK_FILE" "$BASEDIR/build/kiwix-ubuntu-touch-$VERSION.click"
+    echo "Successfully created $BASEDIR/build/kiwix-ubuntu-touch-$VERSION.click"
+else
+    echo "Error: No .click file was generated"
+    echo "Contents of tmp directory:"
+    ls -la "$BASEDIR/tmp"
+    exit 1
+fi
